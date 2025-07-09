e0948bf120c2d8452d042ece2e8487410b176688
Maslinus <sulejmanov.daniyar@mail.ru>
2025-07-09 19:57:18 +0300

    SQLite setup

---
+++ AccountingOfOfficeEquipment/Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using AccountingOfOfficeEquipment.Models;
+using Microsoft.EntityFrameworkCore;
+
+public class AppDbContext : DbContext
+{
+    public DbSet<OfficeEquipment> Equipment { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        if (!optionsBuilder.IsConfigured)
+        {
+            optionsBuilder.UseSqlite("Data Source=equipment.db");
+        }
+    }
+}
---
+++ AccountingOfOfficeEquipment/Data/DataService.cs
@@ -0,0 +1,29 @@
+﻿using AccountingOfOfficeEquipment.Models;
+using System.Collections.ObjectModel;
+
+public class DataService
+{
+    private readonly AppDbContext _context;
+    public ObservableCollection<OfficeEquipment> Equipment { get; private set; }
+
+    public DataService()
+    {
+        _context = new AppDbContext();
+        _context.Database.EnsureCreated();
+        Equipment = new ObservableCollection<OfficeEquipment>(_context.Equipment.ToList());
+    }
+
+    public void AddEquipment(OfficeEquipment eq)
+    {
+        _context.Equipment.Add(eq);
+        _context.SaveChanges();
+        Equipment.Add(eq);
+    }
+
+    public void RemoveEquipment(OfficeEquipment eq)
+    {
+        _context.Equipment.Remove(eq);
+        _context.SaveChanges();
+        Equipment.Remove(eq);
+    }
+}
