ed56eb3807b4b0b3e55f3aff76467b5b6ad228f6
Maslinus <sulejmanov.daniyar@mail.ru>
2025-07-09 18:35:11 +0300

    Models + ViewModals

---
+++ AccountingOfOfficeEquipment/Models/OfficeEquipment.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace AccountingOfOfficeEquipment.Models
+{
+    public enum EquipmentType { Printer, Scanner, Monitor }
+    public enum EquipmentStatus { InUse, InStock, UnderRepair }
+
+    public class OfficeEquipment
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(100)]
+        public string Name { get; set; } = string.Empty;
+        public EquipmentType Type { get; set; }
+        public EquipmentStatus Status { get; set; }
+    }
+}
---
+++ AccountingOfOfficeEquipment/ViewModels/MainViewModel.cs
@@ -0,0 +1,46 @@
+﻿using AccountingOfOfficeEquipment.Models;
+using System.Collections.ObjectModel;
+using System.Windows.Input;
+
+namespace AccountingOfOfficeEquipment.ViewModels
+{
+    public class MainViewModel : BaseViewModel
+    {
+        private readonly DataService _dataService;
+        public ObservableCollection<OfficeEquipment> Equipment => _dataService.Equipment;
+
+        public ICommand AddCommand { get; }
+        public ICommand RemoveCommand { get; }
+
+        private OfficeEquipment? _selectedEquipment;
+        public OfficeEquipment? SelectedEquipment
+        {
+            get => _selectedEquipment;
+            set { _selectedEquipment = value; OnPropertyChanged(); }
+        }
+
+        public MainViewModel()
+        {
+            _dataService = new DataService();
+            AddCommand = new RelayCommand(AddEquipment);
+            RemoveCommand = new RelayCommand(RemoveEquipment, () => SelectedEquipment != null);
+        }
+
+        private void AddEquipment()
+        {
+            var newEq = new OfficeEquipment
+            {
+                Name = "New Equipment",
+                Type = EquipmentType.Printer,
+                Status = EquipmentStatus.InStock
+            };
+            _dataService.AddEquipment(newEq);
+        }
+
+        private void RemoveEquipment()
+        {
+            if (SelectedEquipment != null)
+                _dataService.RemoveEquipment(SelectedEquipment);
+        }
+    }
+}
