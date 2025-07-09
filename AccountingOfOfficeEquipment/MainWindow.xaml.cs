f68e28996234d9f0dacdc6dc07702e97cd7bc086
Maslinus <sulejmanov.daniyar@mail.ru>
2025-07-09 18:34:04 +0300

    SQLite setup + Models + ViewModals

--- AccountingOfOfficeEquipment/MainWindow.xaml.cs
+++ AccountingOfOfficeEquipment/MainWindow.xaml.cs
@@ -1,13 +1,5 @@
-﻿using System.Text;
+﻿using AccountingOfOfficeEquipment.ViewModels;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace AccountingOfOfficeEquipment
 {
@@ -19,7 +11,7 @@ namespace AccountingOfOfficeEquipment
         public MainWindow()
         {
             InitializeComponent();
-            DataContext = new ViewModal();
+            DataContext = new MainViewModel();
         }
     }
 }
---
+++ AccountingOfOfficeEquipment/Models/EnumHelper.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AccountingOfOfficeEquipment.Models
+{
+    public static class EnumHelper
+    {
+        public static Array GetValues(Type enumType) => Enum.GetValues(enumType);
+
+        public static IEnumerable<EquipmentType> EquipmentTypeValues => (EquipmentType[])Enum.GetValues(typeof(EquipmentType));
+        public static IEnumerable<EquipmentStatus> EquipmentStatusValues => (EquipmentStatus[])Enum.GetValues(typeof(EquipmentStatus));
+    }
+}
